bb4493ff796f04cc103a0915e22403ea71a92272
Mihai Miloi <mihai.miloi@gmail.com>
2025-01-28 12:09:21 +0200

    Translate button press message to Romanian

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Ai apasat butonul de atatea ori:',
             ),
             Text(
               '$_counter',
